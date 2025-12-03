a3b410f722bce8532196fc7d197384cae8fa4c92
wang.jianfei.p <wang.jianfei.p@jcbc.co.jp>
2025-12-03 12:50:51 +0900

    ww

---
+++ src/main/java/com/ruoyi/common/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.ruoyi.common.utils;
+/**
+ *  wang.jianfei.p
+ *  2025/12/02
+ */
+
+import java.security.PublicKey;
+
+public class Utils {
+	public static String getName() {
+		return "name";
+	}
+}
